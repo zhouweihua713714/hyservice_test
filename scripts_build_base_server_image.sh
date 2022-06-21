b42a8de79a1f9a827d82a4ebb2f81fdea7482960
jenkins <448083278@example.com>
2022-06-21 16:35:37 +0800

    update hy pipeline scripts

--- scripts_build_base_server_image.sh
+++ scripts_build_base_server_image.sh
@@ -7,10 +7,10 @@ echo 'base image: building ...'
 root="$(if [ "`git rev-parse --show-cdup`" != "" ]; then cd `git rev-parse --show-cdup`; fi; pwd)"
 echo $root
 
-cd "$root/nest-server"
+cd "$root"
 
 echo "system password then namespace password"
 sudo docker login --username=ccnuyan@live.com registry.cn-shanghai.aliyuncs.com
 
-sudo docker build -f Dockerfile.base -t registry.cn-shanghai.aliyuncs.com/mathits/its:base-rest-image .
-sudo docker push registry.cn-shanghai.aliyuncs.com/mathits/its:base-rest-image
+sudo docker build -f Dockerfile.base -t registry.cn-shanghai.aliyuncs.com/mathits/hy:base-rest-image .
+sudo docker push registry.cn-shanghai.aliyuncs.com/mathits/hy:base-rest-image
