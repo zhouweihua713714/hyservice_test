0b0e2d4399e80c6da50ecad8dbbd21616804ba84
tninenine <cczhang319@gmail.com>
2022-08-19 15:57:43 +0800

    task:#HY-38

---
+++ test/policies/operatePolicies.seed.ts
@@ -0,0 +1,115 @@
+import { TesterSeed } from '../testHelper';
+import { CreateUserRetType } from '@/dao/users.dao';
+import { genCodeOfLength } from '@/common/utils/genCodeOfLength';
+import { samples } from '../samples';
+import {
+  Content_Status_Enum,
+  Content_Types_Enum,
+  Education_Level_Enum,
+  User_Types_Enum,
+} from '@/common/enums/common.enum';
+import { Columns } from '@/entities/Columns.entity';
+import { Subjects } from '@/entities/Subjects.entity';
+import { TermTypes } from '@/entities/TermTypes.entity';
+import { Terms } from '@/entities/Terms.entity';
+import { constant } from '@/common/utils/constant';
+const { mobile, password } = samples;
+
+let user: CreateUserRetType;
+let normalUser: CreateUserRetType;
+let columns: Columns[];
+let subjects: Subjects[];
+let policyType: TermTypes;
+let policies: Terms[];
+export type DataType = {
+  user: CreateUserRetType;
+  normalUser: CreateUserRetType;
+  policies: Terms[];
+  columns: Columns[];
+};
+
+export const seed: TesterSeed<DataType> = {
+  up: async (tester) => {
+    user = await tester.usersDao.createUser(
+      { mobile, password, type: User_Types_Enum.Administrator },
+      tester.authService
+    );
+    normalUser = await tester.usersDao.createUser(
+      { mobile, password, type: User_Types_Enum.User },
+      tester.authService
+    );
+    columns = await tester.columnsRepository.save([
+      { id: `C${genCodeOfLength(8)}`, name: '栏目名称', parentId: '0', sequenceNumber: 1 },
+      { id: `C${genCodeOfLength(8)}`, name: '栏目名称1', parentId: '1', sequenceNumber: 1 },
+    ]);
+    subjects = await tester.subjectsRepository.save([
+      { id: `S${genCodeOfLength(8)}`, name: '学科名称', type: Content_Types_Enum.TERM },
+      { id: `S${genCodeOfLength(8)}`, name: '学科名称1', type: Content_Types_Enum.PATENT },
+    ]);
+    policyType = await tester.policyTypesRepository.save({
+      id: `T${genCodeOfLength(8)}`,
+      name: '政策类型名称',
+    });
+    policies = await tester.policiesRepository.save([
+      {
+        id: (new Date().getTime() - 50000).toString(),
+        columnId: columns[1].id,
+        type: policyType.id,
+        name: '政策名称必填',
+        announceNo: '发文号',
+        level: constant.POLICY_LEVEL,
+        institution: '机构名称',
+        educationLevel: [Education_Level_Enum.BASIC, Education_Level_Enum.HIGHER],
+        keyword: '关键字;政策',
+        announcedAt: new Date(),
+        introduction: '简介最多300',
+        region: '中国',
+        url: 'http://baidu.com',
+        ownerId: user.user.id,
+        endedAt: new Date(),
+      },
+      {
+        id: (new Date().getTime() - 40000).toString(),
+        columnId: columns[1].id,
+        type: policyType.id,
+        name: '政策名称必填',
+        announceNo: '发文号',
+        level: constant.POLICY_LEVEL,
+        institution: '机构名称',
+        educationLevel: [Education_Level_Enum.BASIC, Education_Level_Enum.HIGHER],
+        keyword: '关键字;政策',
+        announcedAt: new Date(),
+        introduction: '简介最多300',
+        region: '中国',
+        url: 'http://baidu.com',
+        ownerId: user.user.id,
+        status: Content_Status_Enum.ACTIVE,
+      },
+      {
+        id: (new Date().getTime() - 30000).toString(),
+        columnId: columns[1].id,
+        type: policyType.id,
+        name: '政策名称必填',
+        announceNo: '发文号',
+        level: constant.POLICY_LEVEL,
+        institution: '机构名称',
+        educationLevel: [Education_Level_Enum.BASIC, Education_Level_Enum.HIGHER],
+        keyword: '关键字;政策',
+        announcedAt: new Date(),
+        introduction: '简介最多300',
+        region: '中国',
+        url: 'http://baidu.com',
+        ownerId: user.user.id,
+        updatedAt: new Date(new Date().getTime() - 50000),
+      },
+    ]);
+    return { user, normalUser, policies, columns };
+  },
+  down: async (tester) => {
+    await tester.policiesRepository.delete({});
+    await tester.columnsRepository.delete({});
+    await tester.subjectsRepository.delete({});
+    await tester.usersRepository.delete({});
+    await tester.loginsRepository.delete({});
+  },
+};
