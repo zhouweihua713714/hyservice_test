31fe9880f10951d89a0a28d2a739804a98d3911f
tninenine <cczhang319@gmail.com>
2022-10-14 15:08:06 +0800

    modify migration

--- src/migrations/1665731147677-HY-163.ts
+++ src/migrations/1665731147677-HY-163.ts
@@ -1,10 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class HY1631665716828950 implements MigrationInterface {
-    name = 'HY1631665716828950'
+export class HY1631665731147677 implements MigrationInterface {
+    name = 'HY1631665731147677'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX "public"."keywords_pkey"`);
+        await queryRunner.query(`ALTER TABLE "keywords" DROP CONSTRAINT "PK_4aa660a7a585ed828da68f3c28e"`);
+        await queryRunner.query(`ALTER TABLE "keywords" DROP COLUMN "id"`);
+        await queryRunner.query(`ALTER TABLE "keywords" ADD CONSTRAINT "PK_2b14d429b4ec7cfe3a2bc5a4a0e" PRIMARY KEY ("name", "type")`);
         await queryRunner.query(`ALTER TABLE "keywords" DROP CONSTRAINT "PK_2b14d429b4ec7cfe3a2bc5a4a0e"`);
         await queryRunner.query(`ALTER TABLE "keywords" ADD CONSTRAINT "PK_5f1f83efb667d98cc74acbed850" PRIMARY KEY ("type")`);
         await queryRunner.query(`ALTER TABLE "keywords" DROP COLUMN "name"`);
@@ -24,7 +27,10 @@ export class HY1631665716828950 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "keywords" ADD "name" character varying(128) NOT NULL`);
         await queryRunner.query(`ALTER TABLE "keywords" DROP CONSTRAINT "PK_5f1f83efb667d98cc74acbed850"`);
         await queryRunner.query(`ALTER TABLE "keywords" ADD CONSTRAINT "PK_2b14d429b4ec7cfe3a2bc5a4a0e" PRIMARY KEY ("name", "type")`);
-        await queryRunner.query(`CREATE UNIQUE INDEX "keywords_pkey" ON "keywords" ("name", "type") `);
+        await queryRunner.query(`ALTER TABLE "keywords" DROP CONSTRAINT "PK_2b14d429b4ec7cfe3a2bc5a4a0e"`);
+        await queryRunner.query(`ALTER TABLE "keywords" ADD "id" uuid NOT NULL DEFAULT uuid_generate_v4()`);
+        await queryRunner.query(`ALTER TABLE "keywords" ADD CONSTRAINT "PK_4aa660a7a585ed828da68f3c28e" PRIMARY KEY ("id")`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "keywords_pkey" ON "keywords" ("id") `);
     }
 
 }
