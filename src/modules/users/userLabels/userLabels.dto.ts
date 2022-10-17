4875ff89a066fba47b65f5f41eee7af513efe9fe
tninenine <cczhang319@gmail.com>
2022-10-17 17:15:31 +0800

    modify userLabelsDto

--- src/modules/users/userLabels/userLabels.dto.ts
+++ src/modules/users/userLabels/userLabels.dto.ts
@@ -1,7 +1,15 @@
 import { Labels_Enum, Operate_types_Enum } from '@/common/enums/common.enum';
 import { ReqListQuery } from '@/common/utils/reqListQuery';
 import { ApiProperty, ApiPropertyOptional, PickType } from '@nestjs/swagger';
-import { ArrayMinSize, IsArray, IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class ListLabelTreatiseDto extends PickType(ReqListQuery, ['page', 'size'] as const) {
   @ApiPropertyOptional({ description: '是否获取用户标签,是true,否:false' })
@@ -9,11 +17,11 @@ export class ListLabelTreatiseDto extends PickType(ReqListQuery, ['page', 'size'
   @IsOptional()
   flag: string;
 
-  @ApiPropertyOptional({ description: '标签id' })
+  @ApiPropertyOptional({ description: '标签id', type: String, nullable: true })
   @IsString({ message: 'label 类型错误,正确类型 string' })
   @IsEnum(Labels_Enum)
   @IsOptional()
-  label: string;
+  label: string | null;
 }
 export class OperateLabelTreatisesDto {
   @ApiProperty({ description: '论文id数组', isArray: true })
